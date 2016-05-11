a850106ed8341f26b7fbf9c1fd35db350717f206
rnenrna <hukurou55.1@gmail.com>
2016-05-12 01:54:20 +0900

    Narikoma : Tokin, Narikyo, Narikei, Narigin

---
+++ src/main/java/cive/shogi/Pieces/Tokin.java
@@ -0,0 +1,33 @@
+package cive.shogi.Pieces;
+
+import java.awt.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by yotuba on 16/05/12.
+ * Promoted Fu
+ */
+public class Tokin extends Piece{
+    public Tokin(Point p) {
+        super(p);
+    }
+
+    @Override
+    public String getName() {
+        return "と";
+    }
+
+    @Override
+    public Set<Point> getRuleOfPiece(int player_type) {
+        Set<Point> set = new HashSet<>();
+        PieceFactory factory = new PieceFactory();
+        Piece kin = factory.create(Piece.KIN, this.getPoint());
+        set.addAll(kin.getRuleOfPiece(player_type));
+        return set;
+    }
+    @Override
+    public Integer getTypeOfPiece() {
+        return Piece.TOKIN;
+    }
+}
